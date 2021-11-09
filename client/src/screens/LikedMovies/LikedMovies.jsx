3bc0b9fe7b7bd9baf3bc8c5854f3880c11f18927
Ian-Flaherty <88514194+Ian-Flaherty@users.noreply.github.com>
2021-11-09 01:54:09 -0600

    liked movies lower half styled

--- client/src/screens/LikedMovies/LikedMovies.jsx
+++ client/src/screens/LikedMovies/LikedMovies.jsx
@@ -35,11 +35,14 @@ export default function LikedMovies(props) {
                 <div className="curtains-border">
                 <img className="like-photos" src={like.movie.image_url} alt="nope" style={{ height: '200px', width: '130px', }} />
                 </div>
-                <p>{like.rating ? "yes" : "no"}</p>
                 <div className="buttons-for-likes">
-                <button className="delete-button" onClick={()=>handleLikeDelete(like.id)}>delete</button>
+                  <button className="delete-button" onClick={() => handleLikeDelete(like.id)}>delete</button>
+                  <div className="vote-register">
+                <div className="rating-answer">{like.rating ? "yes" : "no"}
+                </div>
+                </div>
                 <div ClassName="edit-div">
-                <Link className="edit-button" to={`/Edit/${like.id}`}><button>edit</button></Link>
+                <Link  to={`/Edit/${like.id}`}><button className="edit-button">edit</button></Link>
                   </div>
                 </div>
                 </div>
